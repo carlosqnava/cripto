91cb55ad20b46482970e2a1b2bc630fd29073035
unknown <carlosqnava@gmail.com>
2021-05-12 16:50:36 -0500

    .

--- interfaz.py
+++ interfaz.py
@@ -17,11 +17,30 @@ class Interfaz:
         lblArchivo = Label(frame,text="Archivo", font=("Calibri", 14))
         lblArchivo.grid(column=0,row=0, padx=150)
 
-        botonBuscarArchivo = ttk.Button(frame, text="Buscar")
-        botonBuscarArchivo.grid(column=0,row=2)
-
         rutaArchivo = ttk.Entry(frame, width=30)
         rutaArchivo.grid(column=0,row=1, pady=20)
+        
+            
+        def browseFiles():
+            filename = filedialog.askopenfilename(initialdir = "/",
+                                                title = "Select a File",
+                                                filetypes = (("Text files",
+                                                                "*.txt*"),
+                                                            ("all files",
+                                                                "*.*")))
+            
+            # Change label contents
+            return filename
+
+            
+        botonBuscarArchivo = ttk.Button(frame, text="Buscar", command=browseFiles)
+        botonBuscarArchivo.grid(column=0,row=2)
+            
+
+
+        
+
+       
 
         
 
@@ -68,53 +87,10 @@ class Interfaz:
 
         frame.pack(fill="both", expand="yes", padx=20, pady=10)
 
-        def browseFiles():
-            filename = filedialog.askopenfilename(initialdir = "/",
-                                                title = "Select a File",
-                                                filetypes = (("Text files",
-                                                                "*.txt*"),
-                                                            ("all files",
-                                                                "*.*")))
-            
-            # Change label contents
-            label_file_explorer.configure(text="File Opened: "+filename)
+        
 
     # Create the root window
-        window = Tk()
-        
-        # Set window title
-        window.title('File Explorer')
-        
-        # Set window size
-        window.geometry("500x500")
-        
-        #Set window background color
-        window.config(background = "white")
-        
-        # Create a File Explorer label
-        label_file_explorer = Label(window,
-                                    text = "File Explorer using Tkinter",
-                                    width = 100, height = 4,
-                                    fg = "blue")
-        
-            
-        button_explore = Button(window,
-                                text = "Browse Files",
-                                command = browseFiles)
-        
-        button_exit = Button(window,
-                            text = "Exit",
-                            command = exit)
-        
-        # Grid method is chosen for placing
-        # the widgets at respective positions
-        # in a table like structure by
-        # specifying rows and columns
-        label_file_explorer.grid(column = 0, row = 0)
-        
-        button_explore.grid(column = 0, row = 1)
-        
-        button_exit.grid(column = 0,row = 2)
+       
         
 if __name__ == '__main__':
     root = Tk()
